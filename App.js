1297029bfb2dd7a082f25478f03f0b0bbf3eb097
‪yazan nazzal‬‏ <113828456+YazanNazal@users.noreply.github.com>
2022-12-21 13:29:23 +0300

    Simple game

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ import AppLoading from 'expo-app-loading';
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
-  const [guessRounds, setGuessRounds] = useState(0);
+  const [guessRounds, setGuessRounds] = useState(0); 
 
   const [fontsLoaded] = useFonts({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
